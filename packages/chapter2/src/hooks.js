87e0e61ea98cac76990cab1da752b4072b22810b
rose <rosefor2ne@gmail.com>
2024-03-23 00:21:54 +0900

    chapter2 B

--- packages/chapter2/src/hooks.js
+++ packages/chapter2/src/hooks.js
@@ -1,14 +1,32 @@
 export function createHooks(callback) {
+  let currentStateKey = 0;
+  const states = [];
+
   const useState = (initState) => {
-    return [];
-  };
+    
+    const key = currentStateKey;
+    
+    if (states[key] === undefined) {
+      states[key] = initState;
+    }
+
+    const setState = (newState) => {
+      if (newState === states[key]) return;
+      
+      states[key] = newState;
+      
+      resetContext();
+      callback();
+    }
+    currentStateKey += 1;
+    return [ states[key] , setState ];
+  }
 
   const useMemo = (fn, refs) => {
     return fn();
   };
 
   const resetContext = () => {
-
   }
 
   return { useState, useMemo, resetContext };
